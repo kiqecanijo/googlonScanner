919b758799b0cf11337f8fed505d39faf2b5df8b
kiqecanijo <enry1375@gmail.com>
2019-02-18 17:27:08 -0600

    added preposisions, verbs,subjuntives vocabular in progress

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import './App.css'
 import Status from '../status/Status'
 
+const fooLetters = ['u', 'd', 'x', 's', 'm', 'p', 'f']
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      text: '',
       sintax: {
         preposition: {
           words: []
@@ -26,6 +29,7 @@ class App extends Component {
     }
   }
 
+  // HANDLERS
   handleBoxChange(event) {
     this.setState({
       ...this.state,
@@ -33,25 +37,79 @@ class App extends Component {
     })
   }
 
+  //LIFECICLE
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      text:
+        'shoce pq podciy nfwh phfer epgdc dgsloqe do rhfl qhmoixw cmfur qdrulxogji whc ermjdhsx py en yco ienqm wjuln dwuch qinhmjul mjxdqfrnlg iygsex qihmu grewyluhfs ucf us xclpedqmi yrx qinexwo qx rqw wxflpdn rsogxd cpqmxj lgchqdin fdw nwcrus coj nj qplfjnwidg fwdmslqn cwj hysucxdqm ms hdmwpe igxweo sqflo ycqlinro ghu hgecdfj mw xrpmyenq fgixsr fpwcnguieh fclgj ghepqyd jxhwe cejfugn ujxqh ihncrl mlceo udr fm ocxfsjdng sfoqmd pdoymnwxei spqinedf ql ncsepfl icmqsdj chwjlg yiq ifl syejrqd lwnepmcg xlmnfqry ghlyopuncw qx iw sionpux cop dmqpchuyf ojxfqhernm ignpeyf rseoyl emjocsild rfimdy mwd oewgjfr uo irmcunfgx ylduwpsnh xrdng gcxr ng prfmjicud srdueqhgiy nmodwsqijh dcnql'
+    })
+  }
+
+  //LIFECICLE
+  componentDidUpdate() {
+    const alphabet = 'sxocqnmwpfyheljrdgui.'
+    const text = this.state.text.split(' ')
+
+    const order = (a, b) => {
+      const aIndex = alphabet.indexOf(a.slice(0, 1))
+      const bIndex = alphabet.indexOf(b.slice(0, 1))
+
+      return aIndex === bIndex && a.substr(1) + b.substr(1) >= 2
+        ? order(a.substr(1), b.substr(1))
+        : aIndex < bIndex
+    }
+
+    const orderedText = text.sort((a, b) => order(a, b))
+
+    console.log(orderedText)
+
+    //Prepositions
+    console.log(
+      this.state.text
+        .split(' ')
+        .filter(
+          word =>
+            word.length === 6 &&
+            !word.includes('u') &&
+            fooLetters.some(foo => foo === word.slice(-1))
+        )
+    )
+    //verbs
+    console.log(
+      this.state.text
+        .split(' ')
+        .filter(
+          word =>
+            word.length >= 6 && fooLetters.every(foo => foo !== word.slice(-1))
+        )
+    )
+    //verbs subjuntive
+    console.log(
+      this.state.text
+        .split(' ')
+        .filter(
+          word =>
+            word.length >= 6 &&
+            fooLetters.every(foo => foo !== word.slice(0, 1)) &&
+            fooLetters.every(foo => foo !== word.slice(-1))
+        )
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <textarea
-            className="code"
-            type="text-area"
-            rows="20"
-            cols="50"
-            onChange={this.handleBoxChange.bind(this)}
-          />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            <Status values={this.state.sintax} />
-          </a>
-        </header>
+        <textarea
+          className="code"
+          type="text-area"
+          rows="20"
+          cols="50"
+          onChange={this.handleBoxChange.bind(this)}
+          value={this.state.text}
+        />
+
+        <Status values={this.state.sintax} />
       </div>
     )
   }
--- src/status/Status.js
+++ src/status/Status.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Status = ({ values }) => {
-  console.log(values)
   return <h1>status works !</h1>
 }
 export default Status
